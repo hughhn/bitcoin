baddf19d51e329457743c2db094d9a381d83f261
Andrew Chow <achow101-github@achow101.com>
2019-06-27 14:38:01 -0400

    [ci skip] Remove CWallet from IsMine and have CWallet always use ScriptPubKeyMan's IsMine

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -19,7 +19,6 @@
 #include <validationinterface.h>
 #include <wallet/coinselection.h>
 #include <wallet/crypter.h>
-#include <wallet/ismine.h>
 #include <wallet/scriptpubkeyman.h>
 #include <wallet/walletdb.h>
 #include <wallet/walletutil.h>
@@ -1033,6 +1032,8 @@ public:
     bool GetNewDestination(const OutputType type, const std::string label, CTxDestination& dest, std::string& error);
     bool GetNewChangeDestination(const OutputType type, CTxDestination& dest, std::string& error);
 
+    isminetype IsMine(const CTxDestination& dest) const;
+    isminetype IsMine(const CScript& script) const;
     isminetype IsMine(const CTxIn& txin) const;
     /**
      * Returns amount of debit if the input matches the
