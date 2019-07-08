3ab41b223edccdc0da1c1dd2ff97d823f338ac05
Andrew Chow <achow101-github@achow101.com>
2019-07-08 17:28:17 -0400

    Implement GetID for DescriptorScriptPubKeyMan

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -1604,7 +1604,10 @@ bool DescriptorScriptPubKeyMan::CanProvide(const CScript& script, SignatureData&
 
 uint256 DescriptorScriptPubKeyMan::GetID() const
 {
-    return uint256();
+    std::string desc_str = descriptor.descriptor->ToString();
+    uint256 id;
+    CSHA256().Write((unsigned char*)desc_str.data(), desc_str.size()).Finalize(id.begin());
+    return id;
 }
 
 void DescriptorScriptPubKeyMan::SetType(OutputType type, bool internal) {}
