3624f3e989a16fcb90e844a565ef7fda6af2d354
Andrew Chow <achow101-github@achow101.com>
2019-07-11 15:54:07 -0400

    Implement GetReservedDestination in DescriptorScriptPubKeyMan

--- src/wallet/scriptpubkeyman.cpp
+++ src/wallet/scriptpubkeyman.cpp
@@ -1630,7 +1630,10 @@ bool DescriptorScriptPubKeyMan::Encrypt(CKeyingMaterial& master_key_in, WalletBa
 
 bool DescriptorScriptPubKeyMan::GetReservedDestination(const OutputType type, bool internal, CTxDestination& address, int64_t& index, CKeyPool& keypool)
 {
-    return false;
+    std::string error;
+    bool result = GetNewDestination(type, address, error);
+    index = descriptor.next_index - 1;
+    return result;
 }
 
 void DescriptorScriptPubKeyMan::ReturnDestination(int64_t index, bool internal, const CTxDestination& addr)
